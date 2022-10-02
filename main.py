de65ed493627c4b917326fd4a37084e6d8d89be1
chu0802 <r09922104@ntu.edu.tw>
2022-10-02 18:15:22 +0800

    fix bug

--- main.py
+++ main.py
@@ -146,8 +146,8 @@ def main(args):
         if i % args.log_interval == 0:
             writer.add_scalar('LR', lr_scheduler.get_lr(), i)
             writer.add_scalar('Loss/s_loss', s_loss.item(), i)
-            writer.add_scalar('Loss/l_loss', l_loss.item(), i)
-            writer.add_scalar('Loss/soft_loss', soft_loss.item(), i)
+            writer.add_scalar('Loss/l_loss', l_loss.mean().item(), i)
+            writer.add_scalar('Loss/soft_loss', soft_loss.mean().item(), i)
             writer.add_scalar('Loss/t_loss', t_loss.item(), i)
             if 'MME' in args.method or 'CDAC' in args.method:
                 writer.add_scalar('Loss/u_loss', -u_loss.item(), i)
